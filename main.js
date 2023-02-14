183b889f74b02510847ed7dae85de9c20dc91a02
sandrinemfuranzima <75666150+sandrinemfuranzima@users.noreply.github.com>
2023-02-14 13:21:04 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
 import './style.css'
 /** Write your code below this comment */
+
+//chalenge n1
+function test(arr) {
+  var total = 0;
+  for(var i=0;i<=arr.length;i++){
+    if (typeof arr[i] === "number")
+      total=total+arr[i];
+  }
+return total; 
+}
+//chalenge n2
+function removeVowels(str) {
+  return str.replace(/[aeiou]/gi, '');
+}
+  const ans = removeVowels('sandrine');
+  console.log(ans);
+
+
+
+
